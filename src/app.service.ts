6199c119106df069baa7e6fd5c5e7bf34982ea6a
dabibid <davidmeinard.reynoso.s@bulsu.edu.ph>
2023-10-13 14:25:35 +0800

    app.service.ts changes, removed the unecessary imports

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { AuthorsService } from './authors/authors.service';
-import { BooksService } from './books/books.service';
 
 @Injectable()
 export class AppService {
